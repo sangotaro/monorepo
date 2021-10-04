995464cf54c448e09665b437969568447c92ac1c
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2021-10-05 00:48:39 +0900

    chore: add eslint (#10)

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+module.exports = /* eslint-disable-line no-undef */ {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
